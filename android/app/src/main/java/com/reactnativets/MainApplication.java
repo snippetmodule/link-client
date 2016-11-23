fbed438801a52bc79efc5b1c86e6242334999e70
wangfaxi <wangfaxi@cmcm.com>
2016-11-23 11:20:57 +0800

    手动安装：react-native-linear-gradient for android

--- android/app/src/main/java/com/reactnativets/MainApplication.java
+++ android/app/src/main/java/com/reactnativets/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,8 +27,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new ReactNativePushNotificationPackage(),
-            new ReactNativeI18n()
+          new LinearGradientPackage(),
+          new ReactNativePushNotificationPackage(),
+          new ReactNativeI18n()
       );
     }
   };
